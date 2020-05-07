5b39b31b79eea71bb47b326bbcf7e8c7fc188037
binghuan <binghuanlin@gmail.com>
2020-05-07 11:38:16 +0800

    add utils.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log(">> DOMContentLoaded");
     let actionUrl = getParameterByName("action");
---
+++ result.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    let statusImg = document.getElementById("status");
+    let token = getParameterByName("token");
+    let msg = document.getElementById("msg");
+    if (token != null) {
+      statusImg.src = "images/ok.svg";
+      msg.innerText = "Login Successfully!";
+      msg.style.color = "blue";
+    } else {
+      statusImg.src = "images/fail.svg";
+      msg.innerText = "Login Failed!";
+      msg.style.color = "red";
+    }
+
+  });
