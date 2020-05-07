1b9021e4b9100e1346b3359f2284c24c369bbd15
binghuan <binghuanlin@gmail.com>
2020-05-07 13:33:27 +0800

    add function to remember last input.

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log(">> DOMContentLoaded");
+
+    let form = document.getElementById("form");
+
     let actionUrl = getParameterByName("action");
     console.log("action = ", actionUrl);
     if (actionUrl != null) {
-        document.getElementById("form").action = actionUrl;
+        form.action = actionUrl;
     }
 
     let method = getParameterByName("method");
     console.log("method = ", method);
     if (method != null) {
-        document.getElementById("form").method = method;
+        form.method = method;
     }
+
+    document.getElementById("button_submit").addEventListener("click", () => {
+        console.log(">> Submit");
+
+        if (document.getElementById("checkbox_remember").checked) {
+            let account = document.getElementById("inputEmail").value;
+            let password = document.getElementById("inputPassword").value;
+
+            localStorage.account = account;
+            localStorage.password = password;
+        }
+
+        form.submit();
+    });
+
+    restoreLastInput();
 });
+
+function restoreLastInput() {
+    if (localStorage.account != null && localStorage.account.trim().length > 0) {
+        document.getElementById("inputEmail").value = localStorage.account;
+    }
+
+    if (localStorage.password != null && localStorage.password.trim().length > 0) {
+        document.getElementById("inputPassword").value = localStorage.password;
+    }
+}
