4a44f31d7b6ae2266751634086de3581d1b401bf
binghuan <binghuanlin@gmail.com>
2020-05-13 17:36:55 +0800

    add method to refresh token

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     let form = document.getElementById("form");
 
+    restoreLastInput();
+
+    let isExpired = getParameterByName("expired");
+    let password = document.getElementById("inputPassword").value;
+    let account = document.getElementById("inputEmail").value;
+
+    console.log("isExpired:", isExpired, account, password);
+    if (isExpired && 
+            password != null && password.trim().length > 0 && 
+            account && account.trim().length > 0) {
+
+        document.getElementById("progress_view").style.display = "inline-block";
+        form.style.display = "none";
+
+        sessionStorage.forTokenUpdate = true;
+        setTimeout(() => {
+            form.submit();
+        }, 2000);
+
+        return;
+    }
+
+    let timerId = setInterval(() => {
+        if (form.style.opacity < 1.0) {
+            form.style.opacity = parseFloat(form.style.opacity) + 0.1
+        } else {
+            clearInterval(timerId);
+        }
+    }, 100);
+
     let actionUrl = getParameterByName("action");
     console.log("action = ", actionUrl);
     if (actionUrl != null) {
@@ -28,8 +58,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         form.submit();
     });
-
-    restoreLastInput();
 });
 
 function restoreLastInput() {
--- result.js
+++ result.js
@@ -1,3 +1,5 @@
+
+let seconds = 0;
 document.addEventListener("DOMContentLoaded", function (event) {
 
     let statusImg = document.getElementById("status");
@@ -5,15 +7,35 @@ document.addEventListener("DOMContentLoaded", function (event) {
     let msg = document.getElementById("msg");
     if (token != null) {
         statusImg.src = "images/ok.svg";
+        
         msg.innerText = "Login Successfully!";
+        if(sessionStorage.forTokenUpdate == true) {
+            msg.innerText = "Token Updated!";
+            sessionStorage.forTokenUpdate = false;
+        } 
+
         msg.style.color = "blue";
         setCookie("token", token);
+
+        setTimeout(() => {
+
+            $('.mr-auto').text("JWT created!")
+            $('#timestamp').text(`${seconds} seconds ago`)
+            $('.toast-body').text(`accessToken: ${token}`)
+            $('.toast').toast("show")
+            setInterval(() => {
+                seconds += 1;
+                $('#timestamp').text(`${seconds} seconds ago`)
+            }, 1000);
+        }, 200);
 
         let url = `beaker://enmabrowser.api/send?accessToken=${token}&${token}`;
         fetch(url, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, cors, *same-origin
         }).then(response => response.text());
+
+
     } else {
         statusImg.src = "images/fail.svg";
         msg.innerText = "Login Failed!";
